2bd1ca5f5ddf93ed999f2b4b5e79e1a80870a7fb
José Luís Sáez Sánchez <joseluissaez@me.com>
2016-05-07 01:31:20 +0200

    Funcionan todos los gets y post sin filtros

--- routes/api/v1/usuarios.js
+++ routes/api/v1/usuarios.js
@@ -6,6 +6,18 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Usuario = mongoose.model('Usuario');
 
+router.get('/', function (req,res) {
+
+    Usuario.find().exec(function (err,rows) {
+        if(err){
+            next(err);
+            return
+        }
+        res.json({success:true, rows: rows});
+    });
+
+});
+
 router.post('/', function (req,res, next) {
 
     var usuario = new Usuario(req.body);
