cf6e2d63551ca322ea6f531533bb1c1965a50abc
José Luís Sáez Sánchez <joseluissaez@me.com>
2016-05-06 22:10:40 +0200

    Probando Gets en Anuncios y Tokens, se muestran todos bien

--- routes/api/v1/anuncios.js
+++ routes/api/v1/anuncios.js
@@ -6,6 +6,17 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Anuncio = mongoose.model('Anuncio');
 
+router.get('/', function (req,res) {
+    
+    Anuncio.find().exec(function (err, rows) {
+        if (err){
+            next(err);
+            return
+        }
+        res.json({success:true, rows: rows});
+    });
+});
+
 router.post('/', function (req,res, next) {
     
    var anuncio = new Anuncio(req.body);
--- routes/api/v1/tokens.js
+++ routes/api/v1/tokens.js
@@ -6,6 +6,18 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Token = mongoose.model('Token');
 
+router.get('/', function (req,res) {
+    
+    Token.find().exec(function (err,rows) {
+        if(err){
+            next(err);
+            return
+        }
+        res.json({success:true, rows: rows});
+    });
+    
+});
+
 router.post('/', function (req,res, next) {
 
     var token = new Token(req.body);
